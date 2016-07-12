893935bbf1b4fccdf6c90c9b3706c9bed50c08c1
qwerty2586 <qwerty2@students.zcu.cz>
2016-07-13 00:46:00 +0200

    file test v normalnim pythonu

---
+++ file_test.py
@@ -0,0 +1,20 @@
+import zipfile
+import json
+from datetime import datetime
+soubor=zipfile.ZipFile('logs.dump.zip','r')
+seznamSouboru=soubor.namelist()
+JsonSoubor=soubor.open('0/00/000/00001.json','r')
+datum=datetime.strptime(json.loads(soubor.read('0/00/000/00001.json'))[u'asctime'],'%Y-%m-%d %H:%M:%S,%f')
+
+
+print datum
+
+
+nasJson=json.loads(soubor.read('0/00/000/00001.json'))
+date_object = datetime.strptime(nasJson[u'asctime'],'%Y-%m-%d %H:%M:%S,%f')
+
+datum=datetime.strptime(json.loads(soubor.read('0/00/000/00001.json'))[u'asctime'],'%Y-%m-%d %H:%M:%S,%f')
+datum2=datetime.strptime(json.loads(soubor.read('0/00/000/00002.json'))[u'asctime'],'%Y-%m-%d %H:%M:%S,%f')
+
+
+print datum2>datum
