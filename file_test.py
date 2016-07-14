a705464f09a4a513ba3c295a111736234b7c9d5b
qwerty2586 <qwerty2@students.zcu.cz>
2016-07-14 18:33:51 +0200

    dalsi collab

--- file_test.py
+++ file_test.py
@@ -1,6 +1,11 @@
-import zipfile
 import json
+import zipfile
 from datetime import datetime
+
+#import Image, ImageDraw
+
+
+
 soubor=zipfile.ZipFile('logs.dump.zip','r')
 seznamSouboru=soubor.namelist()
 JsonSoubor=soubor.open('0/00/000/00001.json','r')
@@ -17,7 +22,48 @@ datum=datetime.strptime(json.loads(soubor.read('0/00/000/00001.json'))[u'asctime
 datum2=datetime.strptime(json.loads(soubor.read('0/00/000/00002.json'))[u'asctime'],'%Y-%m-%d %H:%M:%S,%f')
 
 
-print datum2>datum
+
+class LogFile:
+    def __init__(self, name, date, method, message) :
+        self.name = name
+        self.date = date
+        self.method = method
+        self.message = message
+
+
+loglist = []
+
+
+zipfilelist = soubor.namelist()
+for x in zipfilelist :
+    myJson = json.loads(soubor.read(x))
+    datum =  datetime.strptime(myJson[u'asctime'],'%Y-%m-%d %H:%M:%S,%f')
+    method=myJson[u'method']
+    message = myJson[u'message']
+    loglist.append(LogFile(x, datum,method,message))
+
+del zipfilelist
+
+
+print loglist[3].name
+print loglist[3].date
+print loglist[3].method
+print loglist[3].message
+
+
+datehistogram = {}
+for x in loglist :
+    datum = x.date
+    datum = datum.replace(second=0, microsecond=0 , minute = 0)
+    if datehistogram.has_key(datum):
+        datehistogram[datum] = datehistogram[datum] + 1
+    else:
+        datehistogram[datum] = 1
+
+def makepichistogram(start,end,width,height):
+
+
+
 
 
 
