148d879818c74e21274762e3022a7c70a69d8b6e
qwerty2586 <qwerty2@students.zcu.cz>
2016-07-24 18:12:19 +0200

    promazani a sirsi sloupecky

--- dayly histogram.py
+++ dayly histogram.py
@@ -6,14 +6,6 @@ from PIL import Image, ImageDraw, ImageFont
 
 # nacteni souboru
 soubor = zipfile.ZipFile('logs.dump.zip', 'r')
-seznamSouboru = soubor.namelist()
-JsonSoubor = soubor.open('0/00/000/00001.json', 'r')
-# formatovani data a casu
-datum = datetime.strptime(json.loads(soubor.read('0/00/000/00001.json'))[u'asctime'], '%Y-%m-%d %H:%M:%S,%f')
-# print datum
-
-nasJson = json.loads(soubor.read('0/00/000/00001.json'))
-date_object = datetime.strptime(nasJson[u'asctime'], '%Y-%m-%d %H:%M:%S,%f')
 
 
 # datum = datetime.strptime(json.loads(soubor.read('0/00/000/00001.json'))[u'asctime'], '%Y-%m-%d %H:%M:%S,%f')
@@ -41,10 +33,6 @@ for x in zipfilelist:
 
 del zipfilelist
 
-# print loglist[3].name
-# print loglist[3].date
-# print loglist[3].method
-# print loglist[3].message
 
 datehistogram = {}
 # spocitani jednotlivych datumu
@@ -78,9 +66,10 @@ def imghistogram(width, height, start=datetime.min, end=datetime.max):
     # vykresleni usecek v obrazku
     for x in datehistogram:
         if (x >= mindate) and (x <= maxdate):
-            lx = (((x - mindate).total_seconds() / 3600) * width) / deltahours
+            lx1 = (((x - mindate).total_seconds() / 3600) * width) / deltahours
+            lx2 = ((((x + timedelta(days=1)) - mindate).total_seconds() / 3600)* width) / deltahours - 2
             ly = (datehistogram[x] * height) / maxcount
-            draw.line((lx, height, lx, height - ly), fill="black")
+            draw.rectangle((lx1, height, lx2, height - ly), fill="blue", outline="black")
 
     return [img, mindate, maxdate, maxcount]
 
@@ -109,5 +98,5 @@ def make_nice_histogram_layout(imghistogramreturn, iteration):
 
 
 image = make_nice_histogram_layout(imghistogram(1000, 500), 1000)
-# image.show()
-image.save("histogram.jpg", format=None)
+image.show()
+#image.save("histogram.jpg", format=None)
