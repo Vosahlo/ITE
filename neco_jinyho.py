acf86cc696f3b5fb4d41d2ee1ced005a6c1e070f
WolfJi <wolfixd80@gmail.com>
2016-08-09 20:50:04 +0200

    Co jsem zmenil.. ucim se-- pardon

--- neco_jinyho.py
+++ neco_jinyho.py
@@ -4,20 +4,17 @@ from datetime import datetime
 from datetime import timedelta
 from PIL import Image, ImageDraw, ImageFont
 
-
 # class pro objekty do listu
 class LogFile:
     def __init__(self, name, date, method, message):
         self.name = name
         self.date = date
-        #      self.dateString = dateString
         self.method = method
         self.message = message
 
-
 # nacteni logu pri spusteni serveru
-def load_logs(filename):
-    loaded_file = zipfile.ZipFile(filename, 'r')
+def load_logs(fileName):
+    loaded_file = zipfile.ZipFile(fileName, 'r')
     zipfilelist = loaded_file.namelist()
     loglist = []
     for x in zipfilelist:
@@ -28,15 +25,13 @@ def load_logs(filename):
         loglist.append(LogFile(x, datum, method, message))
     return loglist
 
-
 # vybrani logu podle zadaneho datumu
-def select_by_date(loglist, start=datetime.min, end=datetime.max):
+def select_by_date(logList, start=datetime.min, end=datetime.max):
     new_log_list = []
-    for x in loglist:
+    for x in logList:
         if (x.date >= start) and (x.date <= end):
             new_log_list.append(x)
     return new_log_list
-
 
 # vybrani logu podle zadaneho textu
 def select_by_text(log_list, pattern):
@@ -45,7 +40,6 @@ def select_by_text(log_list, pattern):
         if (x.message.find(pattern) > -1):
             new_log_list.append(x)
     return new_log_list
-
 
 # vybrani urciteho poctu logu
 def select_by_range(log_list, start, end):
@@ -56,22 +50,20 @@ def select_by_range(log_list, start, end):
         new_log_list.append(log_list[x])
     return new_log_list
 
-
 # vytvoreni mensiho jsonu pro vypsani logu na strance
 def log_list_to_json(log_list):
     json_output = "["
     for x in log_list:
         item = "{" \
                "\"name\" : \"" + x.name + "\"," \
-               "\"date\" : \"" + str(x.date) + "\"," \
-               "\"method\" : \"" + x.method + "\"," \
-               "\"message\" : \"" + x.message + "\"}"
+                                          "\"date\" : \"" + str(x.date) + "\"," \
+                                                                          "\"method\" : \"" + x.method + "\"," \
+                                                                                                         "\"message\" : \"" + x.message + "\"}"
         json_output = json_output + item + ","
 
     json_output = json_output[:-1] + "]"
 
     return json_output
-
 
 # uprava datumu pro vykresleni po hodinach
 def get_histogram_hourly(log_list):
@@ -85,7 +77,6 @@ def get_histogram_hourly(log_list):
             hourly_histogram[datum] = 1
     return hourly_histogram
 
-
 # uprava datumu pro vykresleni po dnech
 def get_histogram_daily(log_list):
     daily_histogram = {}
@@ -98,56 +89,53 @@ def get_histogram_daily(log_list):
             daily_histogram[datum] = 1
     return daily_histogram
 
-
 # hodnoty pouzite pro vykresleni grafu
 hourly = 1
 daily = 2
 
-
 # vytvoreni obrazku
-def imghistogram(width, height, loglist, hourly1_daily2=1):
-    maxcount = 0
+def imgHistogram(width, height, loglist, hourly1_daily2=1):
+    maxCount = 0
 
-    datehistogram = {}
+    dateHistogram = {}
     if hourly1_daily2 == 1:
-        datehistogram = get_histogram_hourly(loglist)
+        dateHistogram = get_histogram_hourly(loglist)
     else:
-        datehistogram = get_histogram_daily(loglist)
+        dateHistogram = get_histogram_daily(loglist)
 
-    mindate = datetime.max
-    maxdate = datetime.min
-    for x in datehistogram:
-        maxcount = max(datehistogram[x], maxcount)
-        if (mindate > x):
-            mindate = x
-        if (maxdate < x):
-            maxdate = x
+    minDate = datetime.max
+    maxDate = datetime.min
+    for x in dateHistogram:
+        maxCount = max(dateHistogram[x], maxCount)
+        if (minDate > x):
+            minDate = x
+        if (maxDate < x):
+            maxDate = x
     img = Image.new('RGB', (width, height), "white")
     draw = ImageDraw.Draw(img)
-    delta = maxdate - mindate
-    deltahours = delta.total_seconds() / 3600
-    if deltahours == 0:  # hotfix
-        deltahours = 1
+    delta = maxDate - minDate
+    deltaHours = delta.total_seconds() / 3600
+    if deltaHours == 0:  # hotfix
+        deltaHours = 1
 
     # vykresleni usecek v obrazku
-    for x in datehistogram:
-        if (x >= mindate) and (x <= maxdate):
-            lx1 = (((x - mindate).total_seconds() / 3600) * width) / deltahours
-            lx2 = ((((x + timedelta(hours=1)) - mindate).total_seconds() / 3600) * width) / deltahours - hourly1_daily2
+    for x in dateHistogram:
+        if (x >= minDate) and (x <= maxDate):
+            lx1 = (((x - minDate).total_seconds() / 3600) * width) / deltaHours
+            lx2 = ((((x + timedelta(hours=1)) - minDate).total_seconds() / 3600) * width) / deltaHours - hourly1_daily2
             if hourly1_daily2 == 2:
-                lx2 = ((((x + timedelta(days=1)) - mindate).total_seconds() / 3600) * width) / deltahours - 2
-            ly = (datehistogram[x] * height) / maxcount
+                lx2 = ((((x + timedelta(days=1)) - minDate).total_seconds() / 3600) * width) / deltaHours - 2
+            ly = (dateHistogram[x] * height) / maxCount
             draw.rectangle((lx1, height, lx2, height - ly), fill="blue", outline="black")
 
-    return [img, mindate, maxdate, maxcount]
-
+    return [img, minDate, maxDate, maxCount]
 
 # ramecek obrazku s popisky
-def make_nice_histogram_layout(imghistogramreturn):
-    old_image = imghistogramreturn[0]
-    min_date = imghistogramreturn[1]
-    max_date = imghistogramreturn[2]
-    max_count = imghistogramreturn[3]
+def make_nice_histogram_layout(imgHistogramReturn):
+    old_image = imgHistogramReturn[0]
+    min_date = imgHistogramReturn[1]
+    max_date = imgHistogramReturn[2]
+    max_count = imgHistogramReturn[3]
     if max_count == 0:
         max_count = 1
     # popis y-osy v grafu
@@ -169,10 +157,10 @@ def make_nice_histogram_layout(imghistogramreturn):
     draw.text((x_shift + corner, old_image.size[1] + corner + 3), unicode(min_date), fill="red")
     draw.text((img.size[0] - corner - draw.textsize(unicode(max_date))[0], old_image.size[1] + corner + 3),
               unicode(max_date), fill="red")
-    carka = 0
-    while (carka <= max_count):
-        ly = old_image.size[1] + corner - ((carka * old_image.size[1]) / max_count)
+    comma = 0
+    while (comma <= max_count):
+        ly = old_image.size[1] + corner - ((comma * old_image.size[1]) / max_count)
         draw.line((x_shift, ly, x_shift + corner, ly), fill="orange")
-        draw.text((x_shift - draw.textsize(unicode(carka))[0], ly), unicode(carka), fill="orange")
-        carka += iteration
+        draw.text((x_shift - draw.textsize(unicode(comma))[0], ly), unicode(comma), fill="orange")
+        comma += iteration
     return img
